fff90693fbb87a0e11ebc8850aff08d59705d6c1
Michal Pravda <michal.pravda@gmail.com>
2016-04-11 09:09:34 +0200

    Exports fixed deck to a fixed file but from two different note types and stripped off any formatting

--- add-ons/export.py
+++ add-ons/export.py
@@ -5,24 +5,27 @@ from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import *
 
+import re
+
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 
 def testFunction():
-    ids = mw.col.findCards("deck:slovicka")
-    with open('d:\\exp.txt', 'w') as f:
-        output = set()
-        for id in ids:
-            card = mw.col.getCard(id)
-            note = card.note()
-            for (name, value) in note.items():
-                if (name == 'Word'):
-                    output.add(value)
-	lis = sorted(list(output))
-        for val in lis:
-            f.write(val + '\n')
-    f.close
-
+  ids = mw.col.findCards("deck:'slovicka nemecky'")
+  with open('d:\\exp.txt', 'w') as f:
+      output = set()
+      for id in ids:
+          card = mw.col.getCard(id)
+          note = card.note()
+          for (name, value) in note.items():
+              if (name == 'Word') or name == 'Text':
+                  value = re.sub('{{c.::(.*?)}}', '\\1', value)
+                  value = value.replace('&nbsp;', '').replace('<div>', '').replace('</div>', '')
+                  output.add(value.encode('utf-8'))
+      lis = sorted(list(output))
+      for val in lis:
+          f.write(val + '\n')
+  f.close
 # create a new menu item, "test"
 action = QAction("test", mw)
 # set it to call testFunction when it's clicked
