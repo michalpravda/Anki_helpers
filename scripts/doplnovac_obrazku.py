255af6c4f21f8aa79072d768190e4f09547650df
Michal Pravda <michal.pravda@gmail.com>
2016-08-15 14:37:57 +0200

    Spravne cleny

--- scripts/doplnovac_obrazku.py
+++ scripts/doplnovac_obrazku.py
@@ -9,11 +9,52 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(lin
 logger = logging.getLogger()
 
 
+def getSlovo(radek):
+    '''
+    vytahne z radku to spravne slovo, ktere me zajima>
+    la mujer => mujer
+    bueno, -a => bueno
+    café m => café
+    '''
+    logger.debug('getSlovo:_%s_'  %radek)
+    result = None
+    if not ' ' in radek:
+        result = radek
+    else:
+        slova = radek.split(' ')
+        #zrus cleny
+        if slova[0] in ['la', 'el']:
+            del (slova[0])
+        if len(slova) == 1:
+            result = slova[0]
+
+        #zrus sklonovani pridavnych jmen, rody
+        else:
+            if slova[1] in ['-a', 'm', 'ž'.decode('windows-1250'), 'm.']:
+                del (slova[1])
+            if len(slova) == 1:
+                result = slova[0].strip(',')
+    if result == None:
+        result = 'error'
+    logger.debug( ' %s -> %s' %(radek, result))
+    return result
+
+deb = False
+if deb:
+    zdroj1 = 'd:\\majkl\\fluent forever\\spanelstina\\Essential Spanish Vocabulary Top 5000_10.txt'
+    zdroj2 = 'd:\\majkl\\fluent forever\\spanelstina\\CZ - Španìlština pro samouky4_10.txt'
+else:
+    zdroj1 = 'd:\\majkl\\fluent forever\\spanelstina\\Essential Spanish Vocabulary Top 5000_pozn.txt'
+    zdroj2 = 'd:\\majkl\\fluent forever\\spanelstina\\CZ - Španìlština pro samouky4.txt'
+
+
+
 pole = {}
-with open ("d:\\majkl\\fluent forever\\spanelstina\\Essential Spanish Vocabulary Top 5000_pozn.txt", 'r') as top5000:
+with open (zdroj1, 'r') as top5000:
     for f in top5000:
    #    absoluto (adj)	"<img src=""paste-4562608183050241.jpg"" />"	absolute	[sound:yandex-ed786d1b-be3786b7-8c793710-a9852788-0ac15d82.mp3] 	574 	1000   adj
-        polozky = f.split('\t')
+        funi = f.decode('utf-8')
+        polozky = funi.split('\t')
 
         logger.debug(polozky[0])
         index = polozky[0].split(' ')[0]
@@ -23,18 +64,22 @@ with open ("d:\\majkl\\fluent forever\\spanelstina\\Essential Spanish Vocabulary
     top5000.close()
 
 
-with open ('d:\\majkl\\fluent forever\\spanelstina\\CZ - Španìlština pro samouky4.txt', 'r') as infile:
+
+with open (zdroj2, 'r') as infile:
 # sbohem	adiós		L01 stitek
     with open ('d:\\majkl\\fluent forever\\spanelstina\\CZ - Španìlština pro samouky_obr.txt', 'w') as outfile:
         for f in infile:
-            polozky_radku = f.split('\t')
-            if polozky_radku[1] in pole:
+            polozky_radku = f.decode('utf-8').split('\t')
+            slovo = getSlovo(polozky_radku[1])
+            if slovo in pole:
                 logger.debug('nasel jsem %s' % polozky_radku[1])
-                r = pole[polozky_radku[1]]
-                outfile.write('%s;%s;%s;%s;%s;%s;%s;%s;\n' %(polozky_radku[0], polozky_radku[1], r[1], r[2], r[3], r[0].split(' ')[1], r[4], polozky_radku[3].strip()))
+                r = pole[slovo]
+                out = '%s;%s;%s;%s;%s;%s;%s;%s;\n' %(polozky_radku[0], polozky_radku[1], r[1], r[2], r[3], r[0].split(' ')[1], r[4], polozky_radku[3].strip())
+                outfile.write(out.encode('utf-8'))
             else:
                 logger.debug('nenasel jsem %s' % polozky_radku[1])
-                outfile.write('%s;%s;%s;%s;%s;%s;%s;%s;\n' %(polozky_radku[0], polozky_radku[1], '', '', '', '', '', polozky_radku[3].strip()))
+                out = '%s;%s;%s;%s;%s;%s;%s;%s;\n' %(polozky_radku[0], polozky_radku[1], '', '', '', '', '', polozky_radku[3].strip())
+                outfile.write(out.encode('utf-8'))
 
 
     infile.close()
